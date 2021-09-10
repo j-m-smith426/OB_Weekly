235eb758ec7123b2adb44c2b402c8a4c330d1604
j-m-smith426 <joabsmith4226@gmail.com>
2021-09-09 20:21:20 -0400

    09_09

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,64 @@
-import React from 'react';
+import React, { Fragment, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Card, CardContent, Grid, List, ListItem, Typography } from '@material-ui/core';
+import '@fontsource/roboto';
 
-function App() {
+const items = [
+  {
+    title: 'Item 1',
+    description: 'The almighty Item #1, the first of it\'s kind'
+  },
+  {
+    title: 'Item 2',
+    description: 'The almighty Item #2, the second of it\'s kind'
+  },
+  {
+    title: 'Item 3',
+    description: 'The almighty Item #3, the middle child'
+  },{
+    title: 'Item 4',
+    description: 'The almighty Item #4, living in the back'
+  },{
+    title: 'Item 5',
+    description: 'The almighty Item #5, the last of it\'s kind'
+  },
+]
+
+
+function App()
+{
+  const [selected, setSelected] = useState<number>();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Grid container>
+      <Grid item xs={6}>
+        <List>
+          {items.map((item, index) => (
+            <ListItem button selected={selected === index} onClick={() => setSelected(index)} >
+            {item.title}
+            </ListItem>
+          ))}
+        </List>
+      </Grid>
+      <Grid item xs={6}>
+        <Card>
+          <CardContent>
+            {selected !== undefined && (
+              <Fragment>
+                <Typography variant="h5">
+                  {items[selected].title}
+                </Typography>
+                <Typography variant="subtitle1" >
+                  {items[selected].description}
+                </Typography>
+              </Fragment>
+          )}
+          </CardContent>
+        </Card>
+      </Grid>
+
+    </Grid>
   );
 }
 
